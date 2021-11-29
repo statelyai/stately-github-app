3bcc2a332de4d1e1657d59b3e816097c2ebb5e6a
Matt Pocock <mattpocockvoice@gmail.com>
2021-11-29 16:10:31 +0000

    Fixed

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -5,8 +5,6 @@ import { probotApp } from "../../lib/app";
 const probot = createProbot();
 
 export default (req, res) => {
-  throw new Error("SOMETHING IS HAPPENING");
-
   return createNodeMiddleware(probotApp, {
     probot,
     webhooksPath: "/api/webhook",
